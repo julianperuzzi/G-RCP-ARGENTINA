c14128ca09fa82d76a549e9e0bf5b46d00d9bd7d
Julian Peruzzi <peru964@gmail.com>
2024-03-30 19:25:00 -0300

    ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop'; // componente para ir al copmienzo
 
-import  Header  from "./components/Header"
-import { Footer } from "./components/Footer"
-import { About } from "./pages/About"
+import Header from './components/Header';
+import { Footer } from './components/Footer';
+import { About } from './pages/About';
+import { Equipo } from './pages/Equipo';
+import { EscuelasPage } from './pages/EscuelasPage';
+import BlogPage from './pages/BlogPage';
 
-import { Equipo } from "./pages/Equipo"
-
-
-import { BrowserRouter,Routes , Route } from "react-router-dom"
-import {EscuelasPage} from "./pages/EscuelasPage"
-import BlogPage from "./pages/BlogPage"
 function App() {
-  
   return (
-
-      <BrowserRouter >
-
-      <Header/>
-
-      <Routes >
-        <Route path="/" element={<About/>} />
-        <Route path="/Nosotros" element={<Equipo/>}/>
-        <Route path="/Escuelas" element={<EscuelasPage/>}/>
-        <Route path="/News" element={<BlogPage/>}/>
-
+    <Router>
+      <Header />
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/Nosotros" element={<Equipo />} />
+        <Route path="/Escuelas" element={<EscuelasPage />} />
+        <Route path="/News" element={<BlogPage />} />
       </Routes>
-
-
-      
-      <Footer/>
-      </BrowserRouter>     
-  )
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
