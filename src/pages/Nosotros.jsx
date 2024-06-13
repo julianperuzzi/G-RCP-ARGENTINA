a66cb4297b19c445fcf01ac2f0e01875e88363b2
Julian Peruzzi <peru964@gmail.com>
2024-06-12 23:05:24 -0300

    Update Nosotros.jsx

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -14,39 +14,81 @@ export const Nosotros = () => {
     <aside className="px-4 py-6 bg-gray-900 text-white animate-fade-in">
 
 
-<img src={otroLogo} alt="Otro logo de la empresa" className="mx-auto mb-4" style={{ maxWidth: '300px' }} />
+<img src={otroLogo} alt="Otro logo de la empresa" className="mx-auto my-8" style={{ maxWidth: '300px' }} />
+
+{/* //---------------------------------------------------------------- */}
 
 
 
-      <div className="flex items-center justify-center bg-gray-800 p-4 my-4 mx-auto sm:mx-40">
-          <img src={empresaLogo} alt="Logo de la empresa" className="w-24 h-24 mr-4" />
-          <div>
-            <h2 className="text-4xl font-bold mb-2 ">Nosotros</h2>
-            <p className=' text-l'>Somos una empresa comprometida con la seguridad y el bienestar de la comunidad.</p>
-          </div>
-        </div>
-      <div className='xl:w-2/3 xl:mx-auto' >
-        
-        
+<div className="mx-auto mt-8 ">
 
-        <div className="mt-4 mx-10 text-[14px] sm:mx-40 md:text-[16px] text-justify">
-          <h2 className="font-bold pt-4 text-xl pb-2">Misión:</h2>
-          <p className="text-l">Nuestra misión es capacitar a instituciones educativas y empresas en técnicas de reanimación cardiopulmonar (RCP), primeros auxilios, soporte vital básico y manejo de desfibriladores externos automáticos (DEA). Nos comprometemos a brindar conocimientos actualizados y prácticos que permitan a los participantes responder de manera efectiva ante emergencias médicas, salvaguardando vidas y promoviendo entornos seguros y preparados para actuar en situaciones críticas.</p>
-          <h2 className="font-bold mt-4 border-t-2 pt-4 pb-2 text-xl">Visión:</h2>
-          <p className="text-l">Nos visualizamos como líderes en la capacitación en RCP, primeros auxilios, soporte vital básico y manejo de DEA para instituciones educativas y empresas. Aspiramos a ser reconocidos por nuestra calidad en la formación, contribuyendo a la creación de una sociedad más preparada y comprometida con la seguridad y el bienestar de sus miembros. Nuestra visión es un futuro donde todos los centros educativos y empresas cuenten con personal capacitado para responder eficazmente ante emergencias médicas, reduciendo así el riesgo de lesiones graves y salvando vidas.</p>
-          <h2 className="font-bold mt-4 border-t-2 pt-4 text-xl pb-2">Objetivo:</h2>
-          <p className="text-l">Buscamos ser participes y generadores de acciones preventivas para la comunidad en general para acabar (o al menos disminuir) las epidemias de las que poco se habla: EL AHOGAMIENTO Y LA MUERTE SÚBITA.</p>
-        </div>
+
+
+  <div className='flex mx-auto justify-center'>
+      <img src={empresaLogo} alt="Logo de la empresa" className="w-32 h-auto " />
+      <div className='pl-3'>
+      <h2 className="text-4xl font-bold my-2">Nosotros</h2>
+      <p className="text-lg max-w-prose">
+        Somos una empresa comprometida con la seguridad y el bienestar de la comunidad.
+      </p>
+      </div>
       </div>
 
+
+
+
+      <div className="sm:w-2/3 mx-auto mt-16 text-left">
+        <div className="mt-12 text-lg md:text-xl text-justify  p-4">
+
+         <div className=' bg-slate-700 rounded-xl p-6 my-8 shadow-xl '>
+          <h2 className="font-bold text-3xl mb-4 text-center">Misión</h2>
+          <p className="mb-8">
+            Nuestra misión es capacitar a instituciones educativas y empresas en técnicas de
+            reanimación cardiopulmonar (RCP), primeros auxilios, soporte vital básico y manejo de
+            desfibriladores externos automáticos (DEA). Nos comprometemos a brindar conocimientos
+            actualizados y prácticos que permitan a los participantes responder de manera efectiva
+            ante emergencias médicas, salvaguardando vidas y promoviendo entornos seguros y
+            preparados para actuar en situaciones críticas.
+          </p>
+          </div>
+
+
+
+          <div className=' bg-slate-700 rounded-xl p-6 my-8 shadow-xl '>
+          <h2 className="font-bold text-3xl mb-4 text-center">Visión</h2>
+          <p className="mb-8">
+            Nos visualizamos como líderes en la capacitación en RCP, primeros auxilios, soporte
+            vital básico y manejo de DEA para instituciones educativas y empresas. Aspiramos a ser
+            reconocidos por nuestra calidad en la formación, contribuyendo a la creación de una
+            sociedad más preparada y comprometida con la seguridad y el bienestar de sus miembros.
+            Nuestra visión es un futuro donde todos los centros educativos y empresas cuenten con
+            personal capacitado para responder eficazmente ante emergencias médicas, reduciendo así
+            el riesgo de lesiones graves y salvando vidas.
+          </p>
+          </div>
+
+
+          <div className=' bg-slate-700 rounded-xl p-6 my-8 shadow-xl '>
+          <h2 className="font-bold text-3xl mb-4 text-center">Objetivo</h2>
+          <p className="mb-8">
+            Buscamos ser partícipes y generadores de acciones preventivas para la comunidad en
+            general para acabar (o al menos disminuir) las epidemias de las que poco se habla: EL
+            AHOGAMIENTO Y LA MUERTE SÚBITA.
+          </p>
+          </div>      
+        </div>
+      </div>  
+      </div>
       
 
+
+{/* //---------------------------------------------------------------- */}
       <div className="flex text-center justify-center ">
-        <h2 className="text-3xl font-bold mb-3 mt-6 xl:mt-32">Nuestro Equipo</h2>
+        <h2 className="text-3xl font-bold mb-3 mt-4 xl:mt-8">Nuestro Equipo</h2>
       </div>
 
       {mostrarMiembros && (
-        <div className="flex flex-wrap justify-center gap-8 lg:w-2/3 lg:mx-auto">
+        <div className="flex flex-wrap justify-center gap-8 sm:w-2/3 mx-auto">
           {/* Aquí van los miembros del equipo */}
           <figure className="md:flex bg-gray-100 rounded-xl p-8 md:p-0 w-full shadow-lg">
             <img
@@ -121,10 +163,13 @@ export const Nosotros = () => {
       
 
       <div className="text-center my-8">
-        <Link to="/Contacto" className="bg-orange-500 hover:bg-black text-white font-bold py-2 px-4 my-6 shadow">
+        <Link to="/Contacto" className="bg-orange-500 hover:bg-black text-white font-bold py-2 px-4 my-6 shadow text-2xl">
           Contáctanos
         </Link>
       </div>
+
+
+
     </aside>
   );
 };
