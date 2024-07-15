c6181ece530238d31c6016043e389f394fb0afed
Julian Peruzzi <peru964@gmail.com>
2024-07-15 13:34:51 -0300

    Update ListonInfinito.jsx

--- src/components/ListonInfinito.jsx
+++ src/components/ListonInfinito.jsx
@@ -18,33 +18,33 @@ const ListonInfinito = () => {
       <h2 className=" text-3xl  text-gray-700 font-bold mx-4 text-center">RESPALDO</h2>
     <div className="md:mb-4 mt-3">
       <Marquee direction="right">
-        <img src={Imagen1} alt="Logo 1" className="w-32 px-3" />
-        <img src={Imagen2} alt="Logo 2" className="w-32 px-3" />
-        <img src={Imagen3} alt="Logo 3" className="w-32 px-3" />
-        <img src={Imagen4} alt="Logo 4" className="w-32 px-3" />
-        <img src={Imagen5} alt="Logo 5" className="w-32 px-3" />
-        <img src={Imagen1} alt="Logo 1" className="w-32 px-3" />
-        <img src={Imagen2} alt="Logo 2" className="w-32 px-3" />
-        <img src={Imagen3} alt="Logo 3" className="w-32 px-3" />
-        <img src={Imagen4} alt="Logo 4" className="w-32 px-3" />
-        <img src={Imagen5} alt="Logo 5" className="w-32 px-3" />
+        <img src={Imagen1} alt="Logo 1" className="w-32 mx-4" />
+        <img src={Imagen2} alt="Logo 2" className="w-32 mx-4" />
+        <img src={Imagen3} alt="Logo 3" className="w-32 mx-4" />
+        <img src={Imagen4} alt="Logo 4" className="w-32 mx-4" />
+        <img src={Imagen5} alt="Logo 5" className="w-32 mx-4" />
+        <img src={Imagen1} alt="Logo 1" className="w-32 mx-4" />
+        <img src={Imagen2} alt="Logo 2" className="w-32 mx-4" />
+        <img src={Imagen3} alt="Logo 3" className="w-32 mx-4" />
+        <img src={Imagen4} alt="Logo 4" className="w-32 mx-4" />
+        <img src={Imagen5} alt="Logo 5" className="w-32 mx-4" />
 
       </Marquee>
       </div>
       <div className="md:mb-4 mt-5">
       <Marquee direction="left">
-        <img src={Imagen6} alt="Logo 6" className="w-32 px-3" />
-        <img src={Imagen7} alt="Logo 7" className="w-32 px-3" />
+        <img src={Imagen6} alt="Logo 6" className="w-32 mx-4" />
+        <img src={Imagen7} alt="Logo 7" className="w-32 mx-4" />
+       {/* <img src={Imagen8} alt="Logo 8" className="w-32 px-3" /> */}
+        <img src={Imagen9} alt="Logo 9" className="w-32 mx-4" />
+        <img src={Imagen10} alt="Logo 10" className="w-32 mx-4" />
+        <img src={Imagen11} alt="Logo 11" className="w-32 mx-4"/>
+        <img src={Imagen6} alt="Logo 6" className="w-32 mx-4" />
+        <img src={Imagen7} alt="Logo 7" className="w-32 mx-4" />
         {/* <img src={Imagen8} alt="Logo 8" className="w-32 px-3" /> */}
-        <img src={Imagen9} alt="Logo 9" className="w-32 px-3" />
-        <img src={Imagen10} alt="Logo 10" className="w-32 px-3" />
-        <img src={Imagen11} alt="Logo 11" className="w-32 px-3" />
-        <img src={Imagen6} alt="Logo 6" className="w-32 px-3" />
-        <img src={Imagen7} alt="Logo 7" className="w-32 px-3" />
-        {/* <img src={Imagen8} alt="Logo 8" className="w-32 px-3" /> */}
-        <img src={Imagen9} alt="Logo 9" className="w-32 px-3" />
-        <img src={Imagen10} alt="Logo 10" className="w-32 px-3" />
-        <img src={Imagen11} alt="Logo 11" className="w-32 px-3" />
+        <img src={Imagen9} alt="Logo 9" className="w-32 mx-4" />
+        <img src={Imagen10} alt="Logo 10" className="w-32 mx-4" />
+        <img src={Imagen11} alt="Logo 11" className="w-32 mx-4" />
 
       </Marquee>
       </div>
