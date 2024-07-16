baa3ebe8be43cd38b04fa89f62e202226c488c5d
Julian Peruzzi <peru964@gmail.com>
2024-07-15 22:26:28 -0300

    Update ContactButton.jsx

--- src/components/ContactButton.jsx
+++ src/components/ContactButton.jsx
@@ -9,9 +9,9 @@ const ContactButton = () => {
       href="https://wa.me/+54 9 2645 66-7981" // Reemplaza con tu número de WhatsApp
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-4 right-4 bg-green-500 text-white p-5 rounded-full shadow-lg flex items-center justify-center animate-bounce hover:bg-blue-600 transition duration-300 ease-in-out z-20"
+      className="fixed bottom-4 right-4 bg-green-500 text-white p-4 rounded-full shadow-lg flex items-center justify-center animate-bounce hover:bg-blue-600 transition duration-300 ease-in-out z-20"
     >
-      <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+      <FontAwesomeIcon icon={faWhatsapp} size="xl" />
     </a>
   );
 };
