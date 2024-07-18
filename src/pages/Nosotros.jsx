aef56e094bfb4efb16cee227e6cf0f3cc35fc452
Julian Peruzzi <peru964@gmail.com>
2024-07-18 10:45:25 -0300

    Update Nosotros.jsx

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -29,15 +29,11 @@ export const Nosotros = () => {
         bgImageStyle={{ objectFit: 'cover' }}
       >
         <div className="flex flex-col items-center text-center justify-center md:mt-24 py-10 md:py-0" data-aos="zoom-in">
-        <p className="text-2xl px-10 text-white border-b border-gray-400 "> GRCP ARGENTINA
+        <p className="md:text-2xl text-l px-10 text-white border-b border-gray-400 "> GRCP ARGENTINA
           </p>
-          <h2 className={`lg:text-5xl text-3xl font-bold p-4 bg-clip-text md:w-2/3 text-white`}>
-          GRUPO DE RESCATE,
+          <h2 className={`lg:text-5xl text-2xl font-bold p-4 bg-clip-text md:w-2/3 text-white`}>GRUPO DE RESCATE, CAPACITACIÓN Y PREVENCIÓN 
           </h2>
-          <h2 className={`lg:text-5xl text-3xl font-bold pb-4 bg-clip-text md:w-2/3 text-white`}>
-          CAPACITACIÓN Y PREVENCIÓN
-          </h2>
-          <p className="my-2 text-xl px-10 text-white border-t border-gray-400 pt-4 uppercase">
+          <p className="my-2 md:text-xl text-sm px-10 text-white border-t border-gray-400 pt-4 uppercase">
             Capacitación, Prevención y Salvamento en Emergencias
           </p>
         </div>
@@ -46,8 +42,8 @@ export const Nosotros = () => {
 
       <div className="mx-auto mt-8">
 
-        <div className='flex mx-auto justify-center items-start'>
-          <img src={empresaLogo} alt="Logo de la empresa" className="w-36 h-auto" />
+        <div className='flex mx-auto justify-center md:items-start items-center px-4'>
+          <img src={empresaLogo} alt="Logo de la empresa" className="md:w-36 w-24 h-auto" />
           <div className='pl-4'>
             <h2 className="text-4xl font-bold my-2 py-2 border-orange-500 border-b-2">Nosotros</h2>
             <p className="text-lg max-w-prose">
@@ -60,7 +56,7 @@ export const Nosotros = () => {
           <div className="mt-8 text-lg md:text-xl p-4 md:w-10/12 mx-auto">
             {/* Sección Misión */}
             <div className="flex flex-col md:flex-row p-6 my-8 items-center card" data-aos="fade-up">
-              <img src={misionImage} alt="Misión" className="w-full md:w-1/2 md:h-96 h-40 mb-4 md:mb-0 md:mr-8  shadow-md" />
+              <img src={misionImage} alt="Misión" className="w-full md:w-1/2 md:h-96 h-48 mb-4 md:mb-0 md:mr-8  shadow-md" />
               <div className="card-content">
                 <h2 className="font-bold md:text-5xl text-2xl mb-4 md:text-left">Misión</h2>
                 <p className="">
@@ -73,7 +69,7 @@ export const Nosotros = () => {
 
             {/* Sección Visión */}
             <div className="flex flex-col md:flex-row-reverse p-6 my-8 items-center card" data-aos="fade-up">
-              <img src={visionImage} alt="Visión" className="w-full md:w-1/2 h-96 mb-4 md:mb-0 md:ml-8  shadow-md" />
+              <img src={visionImage} alt="Visión" className="w-full md:w-1/2 md:h-96 h-48 mb-4 md:mb-0 md:ml-8  shadow-md" />
               <div className="card-content">
                 <h2 className="font-bold md:text-5xl text-2xl mb-4 md:text-left">Visión</h2>
                 <p className="">
@@ -87,7 +83,7 @@ export const Nosotros = () => {
 
             {/* Sección Objetivo */}
             <div className="flex flex-col md:flex-row p-6 my-8 items-center card" data-aos="fade-up">
-              <img src={objetivoImage} alt="Objetivo" className="w-full md:w-1/2 h-96 mb-4 md:mb-0 md:mr-8 shadow-md" />
+              <img src={objetivoImage} alt="Objetivo" className="w-full md:w-1/2 md:h-96 h-48 mb-4 md:mb-0 md:mr-8 shadow-md" />
               <div className="card-content">
                 <h2 className="font-bold md:text-5xl text-2xl mb-4 md:text-left">Objetivo</h2>
                 <p className="">
