79b5ee61354a7eeaff2fb938c41bf044a969fdec
Julian Peruzzi <peru964@gmail.com>
2024-07-17 23:18:26 -0300

    Update Servicios.jsx

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -15,14 +15,14 @@ import 'aos/dist/aos.css';
 
   return (
     <div className="" data-aos="fade-right">
-      <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center text-slate-800">Servicios de G-RCP Argentina</h2>
+      <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center text-slate-800 uppercase">Servicios de G-RCP Argentina</h2>
 
       {/* Servicios */}
       <Link to="/Servicios" >
       <div className="bg-white md:shadow-lg overflow-hidden flex md:flex-row flex-col-reverse hover:scale-105 transition duration-500 md:h-96">
       
         <div className="p-4 md:w-1/2">
-        <h3 className='text-2xl font-bold mb-2 text-orange-500'>Nuestros Servicios</h3>
+        <h3 className='text-2xl font-bold mb-2 text-orange-500 uppercase'>Nuestros Servicios</h3>
           <p className="text-gray-700 text-lg mb-4">
             Ofrecemos soluciones integrales de capacitación en primeros auxilios y RCP adaptadas a diferentes sectores:
           </p>
