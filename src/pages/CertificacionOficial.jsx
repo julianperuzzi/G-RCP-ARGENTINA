3923f2c81efe2e4e757e4b3aa7c6d2c81fd3b18a
Julian Peruzzi <peru964@gmail.com>
2024-07-23 13:20:17 -0300

    Update CertificacionOficial.jsx

--- src/pages/CertificacionOficial.jsx
+++ src/pages/CertificacionOficial.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import imgCertificado from '../assets/img certificacion/Certificados  RCP Company.png';
+import imgCertificado from '../assets/img certificacion/Certificados  RCP Company (1).png';
 
 const CertificacionOficial = () => {
   return (
@@ -7,7 +7,7 @@ const CertificacionOficial = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-5">
           <div className="lg:w-1/2 mb-8 lg:mb-0">
-            <h2 className="text-4xl font-bold text-gray-800 mb-4">Entrega de Certificación Oficial Avalada por Organismos Especializados</h2>
+            <h2 className="text-4xl font-bold text-gray-800 mb-4">Entrega de Certificación </h2>
             <p className="text-lg text-gray-700 mb-6">
               Garantizamos estándares de excelencia en formación para primeros auxilios, rescate y técnicas médicas. Al obtener nuestra certificación, los participantes demuestran competencia validada por expertos en el campo, reflejando nuestro compromiso con la seguridad y el bienestar. Estamos comprometidos en proporcionar capacitación integral que permita enfrentar cualquier emergencia con profesionalismo y confianza, preparando a nuestros estudiantes para ser agentes de cambio en sus comunidades y salvaguardar vidas en situaciones críticas. Con una combinación de teoría sólida y práctica guiada por instructores altamente capacitados, nuestra certificación oficial representa un hito significativo en el desarrollo profesional y personal de nuestros participantes, abriendo oportunidades laborales y fortaleciendo la red de seguridad comunitaria.
             </p>
